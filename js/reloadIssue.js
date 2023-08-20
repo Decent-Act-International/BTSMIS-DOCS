b89defc7117e12b6f6a714fe463859bce1e12c3e
sohan110111 <sohan110111@gmail.com>
2023-08-20 16:22:19 +0600

    image file name extension change

--- js/reloadIssue.js
+++ js/reloadIssue.js
@@ -20,6 +20,29 @@ window.onload = function () {
   // for new tab issue;
   const currentUrl = window.location.href;
   const constructUrl = currentUrl.split("#");
-  const value = constructUrl[1];
+  let value = constructUrl[1];
+
+  // conditional statement for table of content
+
+  if (value === "add_workers" || value === "workers_list") {
+    try {
+      value = "Worker";
+    } catch (error) {
+      console.log(error);
+    }
+  } else if (value === "add_factory" || value === "factory_list") {
+    value = "Factory";
+  } else if (value === "add_union" || value === "union_list") {
+    value = "Union";
+  } else if (value === "add_association" || value === "association_list") {
+    value = "Association";
+  } else if (value === "add_users" || value === "users_list") {
+    value = "Users";
+  } else if (value === "national_federation" || value === "sectoral_federation" || value === "department" || value === "designation" || value === "sector" || value === "education" || value === "religion" || value === "employment_contract") {
+    value = "Settings";
+  } else if (value === "user-role" || value === "approval_list") {
+    value = "Users";
+  }
+
   display(value);
 };
